b99c04109ae52ef29f73eafee055121913098322
Jafer Abdella <jaferabdella674@mail.com>
2026-08-24 17:47:17 +0300

    Fix GitHub Pages routing

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+   base: "/jaaf-coffee-shop/",
+
   plugins: [
     react(),
     tailwindcss(),
   ],
-    base: "/jaaf-coffee-shop/",
-
+   
 });
